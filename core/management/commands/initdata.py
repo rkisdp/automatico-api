55d95d82e52d275e1f404c4166dbcd6056977c75
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-01-08 11:28:19 -0400

    Fixed django initdata command

--- core/management/commands/initdata.py
+++ core/management/commands/initdata.py
@@ -1,8 +1,9 @@
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
-    help = "Init data for project"
-
     def handle(self, *args, **options):
-        pass
+        self.fill_verification_code_types()
+
+    def fill_verification_code_types(self):
+        call_command("loaddata", "verification_code_types")
