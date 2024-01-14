e8f38c09abc3f02715be1f2082f0abf7684a9ba5
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-01-14 15:03:24 -0400

    API version v0.4.0

--- app/settings.py
+++ app/settings.py
@@ -116,7 +116,7 @@ CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "AutoMático API",
-    "VERSION": "v0.3.0",
+    "VERSION": "v0.4.0",
     "TOS": None,
     "LICENSE": None,
     "SERVE_INCLUDE_SCHEMA": False,
