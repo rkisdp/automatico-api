6d2c18fdcdb6f8fa11ea60e83a05a42b590da9ff
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-01-21 01:55:07 -0400

    Disabling user on delete user endpoint

--- user/views/user_profile_view.py
+++ user/views/user_profile_view.py
@@ -1,15 +1,23 @@
 from importlib import import_module
 
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 SCHEMA_NAME = "user"
 
 
 @extend_schema(tags=[SCHEMA_NAME])
-class UserProfileView(RetrieveUpdateAPIView):
+class UserProfileView(RetrieveUpdateDestroyAPIView):
+    @extend_schema(responses={"204": None})
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
     def get_object(self):
         return self.request.user
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
 
     def get_serializer_class(self):
         version = self._get_version()
