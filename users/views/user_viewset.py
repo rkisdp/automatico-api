01824e1ddecb503d58445f6881df0577f7931070
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-01-21 01:15:39 -0400

    Added omar.nunez versioning

--- users/views/user_viewset.py
+++ users/views/user_viewset.py
@@ -1,9 +1,9 @@
+from importlib import import_module
+
 from django.contrib.auth import get_user_model
 from drf_spectacular.utils import extend_schema
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
-
-from ..serializers import UserSerializer
 
 SCHEMA_NAME = "users"
 
@@ -15,8 +15,24 @@ class UserViewSet(
     GenericViewSet,
 ):
     queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
     lookup_field = "id"
     ordering = ("id",)
     ordering_fields = ["first_name", "last_name"]
     search_fields = ["first_name", "last_name"]
+
+    def get_serializer_class(self):
+        version = self._get_version()
+        serializer = self._get_versioned_serializer_class(version)
+        return serializer
+
+    def _get_version(self):
+        try:
+            version = self.request.version
+        except Exception:
+            version, _ = self.determine_version(self.request)
+        return version
+
+    def _get_versioned_serializer_class(self, version):
+        module = import_module(f"users.serializers.{version.replace('.', '_')}")
+        serializer = getattr(module, "UserSerializer")
+        return serializer
