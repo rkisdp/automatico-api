eb28fd725d902b3432328293c4a12389a285303c
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-02-07 17:37:12 -0400

    Added retrieve new workshops endpoint

---
+++ workshops/views/workshop_list_new_view.py
@@ -0,0 +1,110 @@
+from datetime import timedelta
+
+from django.utils import timezone
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from rest_framework import mixins
+from rest_framework.filters import SearchFilter
+from rest_framework.settings import api_settings
+
+from core.generics import GenericAPIView
+from vehicles.models import VehicleBrandModel
+from workshops.filters import WorkshopFilterSet
+from workshops.models import SpecialityModel, WorkshopModel
+
+SCHEMA_TAGS = ("workshops",)
+
+
+@extend_schema(tags=SCHEMA_TAGS)
+class WorkshopListNewView(
+    mixins.ListModelMixin,
+    GenericAPIView,
+):
+    queryset = WorkshopModel.objects.all()
+    ordering = ("id",)
+    filterset_class = WorkshopFilterSet
+    filter_backends = api_settings.DEFAULT_FILTER_BACKENDS + [SearchFilter]
+    search_fields = ("name", "specialities__name", "brands__name")
+    ordering_fields = ("id", "name")
+
+    @extend_schema(
+        operation_id="list-workshops",
+        summary="List workshops",
+        description=(
+            "Lists all workshops in the order that they were created."
+            "\n\n"
+            "**Note**: Pagination is powered exclusively by the `page` parameter. "
+            "Use the [Link header]"
+            "(https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Link) "
+            "to get the URL for the next page of workshops."
+        ),
+        parameters=(
+            OpenApiParameter(
+                name="brands",
+                description="Filter by vehicle brands.",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                enum=VehicleBrandModel.objects.values_list("name", flat=True),
+                many=True,
+                explode=False,
+            ),
+            OpenApiParameter(
+                name="specialities",
+                description="Filter by specialities.",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                enum=SpecialityModel.objects.values_list("name", flat=True),
+                many=True,
+                explode=False,
+            ),
+            OpenApiParameter(
+                name="ordering",
+                description="Which field to use when ordering the results.",
+                type=OpenApiTypes.STR,
+                many=True,
+                explode=False,
+                enum=(
+                    field
+                    for pair in zip(
+                        ordering_fields,
+                        (f"-{field}" for field in ordering_fields),
+                    )
+                    for field in pair
+                ),
+                default="id",
+            ),
+            OpenApiParameter(
+                name="page",
+                description="The page number of the results to fetch.",
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                default=1,
+            ),
+            OpenApiParameter(
+                name="page_size",
+                description=(
+                    "The number of results to return per page (max 100)."
+                ),
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                default=api_settings.PAGE_SIZE,
+            ),
+            OpenApiParameter(
+                name="X-User-Location",
+                description=(
+                    "The user's location in the format `<latitude>,<longitude>`."
+                ),
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.HEADER,
+                pattern=r"^-?\d+(\.\d+)?,-?\d+(\.\d+)?$",
+            ),
+        ),
+    )
+    def get(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            created_at__gte=timezone.now() - timedelta(days=30)
+        )
