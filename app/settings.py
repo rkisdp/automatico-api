8a72345b0b384eaccc28c947d854058c701017e2
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-02-14 11:41:25 -0400

    Updated API version. Allowed versions are major ones, not minor

--- app/settings.py
+++ app/settings.py
@@ -71,8 +71,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PARSER_CLASSES": ("core.parsers.AutoMaticoJSONParser",),
     "DEFAULT_RENDERER_CLASSES": ("core.renderers.AutoMaticoJSONRenderer",),
     "DEFAULT_VERSIONING_CLASS": "core.versioning.XAutoMaticoAPIVersioning",
-    "DEFAULT_VERSION": "v0.11",
-    "ALLOWED_VERSIONS": ("v0.11", "v0.10", "v0.9"),
+    "DEFAULT_VERSION": "v0",
+    "ALLOWED_VERSIONS": ("v0",),
     "VERSION_PARAM": "version",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
@@ -121,7 +121,7 @@ CSRF_TRUSTED_ORIGINS = env.tuple("CSRF_TRUSTED_ORIGINS", default=())
 SPECTACULAR_SETTINGS = {
     "TITLE": "AutoMático API",
     "DESCRIPTION": "AutoMático API",
-    "VERSION": "v0.11.0",
+    "VERSION": "v0.12.0",
     "TOS": None,
     "LICENSE": None,
     "SERVE_INCLUDE_SCHEMA": False,
