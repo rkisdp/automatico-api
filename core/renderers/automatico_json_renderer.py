43cf186124270d1fed19b17900e1a8c9e65129b9
OmarNunezG <69877860+OmarNunezG@users.noreply.github.com>
2024-03-04 11:23:43 -0400

    AutoMaticoJsonRenderer pretty prints the result by default with an indent of 2

--- core/renderers/automatico_json_renderer.py
+++ core/renderers/automatico_json_renderer.py
@@ -1,6 +1,23 @@
-from rest_framework.renderers import JSONRenderer
+from rest_framework.renderers import JSONRenderer, zero_as_none
+from rest_framework.utils.mediatypes import parse_header_parameters
 
 
 class AutoMaticoJSONRenderer(JSONRenderer):
     media_type = "application/vnd.automatico+json"
     format = "automatico+json"
+
+    def get_indent(self, accepted_media_type, renderer_context):
+        """S"""
+        if accepted_media_type:
+            # If the media type looks like 'application/vnd.automatico+json;
+            # indent=4', then pretty print the result.
+            # Note that we coerce `indent=0` into `indent=None`.
+            base_media_type, params = parse_header_parameters(
+                accepted_media_type
+            )
+            try:
+                return zero_as_none(max(min(int(params["indent"]), 8), 0))
+            except (KeyError, ValueError, TypeError):
+                pass
+
+        return renderer_context.get("indent", 2)
